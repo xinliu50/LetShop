7d4b91a9c13d6de166f07ac05f52acd6b2f52b6d
xinliu <xinliu@owners-MacBook-Air.loca>
2019-11-02 17:41:25 -0400

    adding product detail fragment

--- app/src/main/java/com/example/letshop/ui/productDetails/productDetailsFragment.java
+++ app/src/main/java/com/example/letshop/ui/productDetails/productDetailsFragment.java
@@ -2,6 +2,7 @@ package com.example.letshop.ui.productDetails;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,12 +12,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import com.example.letshop.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class productDetailsFragment extends Fragment {
 
     private ProductDetailsViewModel mViewModel;
+    private View root;
+
+    private FloatingActionButton addToCartBtn;
+    private ImageView productImage;
+    private ElegantNumberButton numberButton;
+    private TextView productPrice, productDescription,productName;
 
     public static productDetailsFragment newInstance() {
         return new productDetailsFragment();
@@ -25,7 +36,19 @@ public class productDetailsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.product_details_fragment, container, false);
+       root = inflater.inflate(R.layout.product_details_fragment, container, false);
+
+       InitialUI();
+       return root;
+    }
+
+    private void InitialUI() {
+        addToCartBtn = (FloatingActionButton)root.findViewById(R.id.add_product_to_cart_btn);
+        numberButton = (ElegantNumberButton)root.findViewById(R.id.number_btn);
+        productImage = (ImageView)root.findViewById(R.id.product_image_details);
+        productName = (TextView)root.findViewById(R.id.product_name_details);
+        productDescription = (TextView)root.findViewById(R.id.product_description_details);
+        productPrice = (TextView)root.findViewById(R.id.product_price_details);
     }
 
     @Override
