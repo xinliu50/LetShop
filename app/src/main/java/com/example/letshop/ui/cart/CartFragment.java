02ae6eb6470a7b6f832860a42d83cae6f89e6347
xinliu <xinliu@owners-MacBook-Air.loca>
2019-11-02 20:59:12 -0400

    design cart list

--- app/src/main/java/com/example/letshop/ui/cart/CartFragment.java
+++ app/src/main/java/com/example/letshop/ui/cart/CartFragment.java
@@ -7,16 +7,28 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.letshop.R;
+
+import org.w3c.dom.Text;
 
 public class CartFragment extends Fragment {
 
     private CartViewModel mViewModel;
+    private View root;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private Button NextProcessBtn;
+    private TextView txtTotalAmount;
 
     public static CartFragment newInstance() {
         return new CartFragment();
@@ -25,7 +37,19 @@ public class CartFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.cart_fragment, container, false);
+        root = inflater.inflate(R.layout.cart_fragment, container, false);
+        InitialUI();
+        return root;
+    }
+
+    private void InitialUI() {
+        recyclerView = root.findViewById(R.id.cart_list);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        NextProcessBtn = (Button)root.findViewById(R.id.next_process_btn);
+        txtTotalAmount = (TextView)root.findViewById(R.id.total_price);
     }
 
     @Override
